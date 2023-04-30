09f843ea4c01a34c0ceee12cebab6ceec3a3ef66
André Nascimento <andre4.20nascimento@outlook.com>
2023-04-30 17:17:00 -0300

    Simulador de Luz de Advertência

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.Clear();
+
+string T,P,R;
+
+Console.WriteLine("---Simulador de Luz de Advertência---\n");
+
+Console.WriteLine("*******************************");
+Console.WriteLine("***(A)tivado ** (D)esativado***");
+Console.WriteLine("*******************************\n");
+
+Console.Write("Sensor de temperatura...: ");
+T = Console.ReadLine()!.Trim().Substring(0, 1).ToUpper();
+
+Console.Write("Sensor de pressão.......: ");
+P = Console.ReadLine()!.Trim().Substring(0, 1).ToUpper();
+
+Console.Write("Sensor de rotação.......: ");
+R = Console.ReadLine()!.Trim().Substring(0, 1).ToUpper();
+
+Console.WriteLine();
+
+
+if(T == "A" || P == "A" || R == "D" )
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("A luz de advertência estará ATIVADA.");
+    Console.ResetColor();
+}
+if (T != "A" && P != "A" && R != "D")
+{
+    Console.WriteLine("A luz de advertência estará DESATIVADA.");
+}
